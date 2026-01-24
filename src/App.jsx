ccdf3ca3adfd7c7d020dfb11e55ce82649925dd8
BS4002 <bakkousalma@gmail.com>
2026-01-25 00:14:35 +0100

    Configuration du routing de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Houses from "./pages/Houses";
+import HouseDetails from "./pages/HouseDetails";
+import Checkout from "./pages/Checkout";
+import Contact from "./pages/Contact";
+import Favorites from "./pages/Favorites";
+import AdminLogin from "./admin/AdminLogin";
+import AdminDashboard from "./admin/AdminDashboard";
+import AddHouse from "./admin/AddHouse";
+import Reservations from "./admin/Reservations";
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/maisons" element={<Houses />} />
+        <Route path="/maisons/:id" element={<HouseDetails />} />
+        <Route path="/checkout/:id" element={<Checkout />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/favoris" element={<Favorites />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/ajouter" element={<AddHouse />} />
+        <Route path="/admin/reservations" element={<Reservations />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
+
---
+++ src/pages/Houses.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Houses() {
+  return (
+    <div>
+    </div>
+  )
+}
